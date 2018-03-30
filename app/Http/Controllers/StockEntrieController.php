a8b017ee8aa10f0ca6414f008875fdc3e0b2bc9e
Gieandes Silva <contato@gieandessilva.com>
2018-03-29 22:10:42 -0300

    Criação da Model, Controller, Migrations - Stock Entries

---
+++ app/Http/Controllers/StockEntrieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StockEntrie;
+use Illuminate\Http\Request;
+
+class StockEntrieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StockEntrie  $stockEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StockEntrie $stockEntrie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StockEntrie  $stockEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StockEntrie $stockEntrie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StockEntrie  $stockEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StockEntrie $stockEntrie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StockEntrie  $stockEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StockEntrie $stockEntrie)
+    {
+        //
+    }
+}
