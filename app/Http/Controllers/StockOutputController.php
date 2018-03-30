fdf256dc4a73f5183886deb4903b21497fc0417e
Gieandes Silva <contato@gieandessilva.com>
2018-03-29 22:12:03 -0300

    Criação da Model, Controller, Migrations - Stock Outputs

---
+++ app/Http/Controllers/StockOutputController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StockOutput;
+use Illuminate\Http\Request;
+
+class StockOutputController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StockOutput  $stockOutput
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StockOutput $stockOutput)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StockOutput  $stockOutput
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StockOutput $stockOutput)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StockOutput  $stockOutput
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StockOutput $stockOutput)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StockOutput  $stockOutput
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StockOutput $stockOutput)
+    {
+        //
+    }
+}
